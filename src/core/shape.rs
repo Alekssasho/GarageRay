c34e0063c2eb4db745269ee117cbd4cf39bd1efa
Alekssasho <aleksandar.angelovv@gmail.com>
2019-06-12 08:25:22 +0300

    Finished shape interface.
    Book to page 130

--- src/core/shape.rs
+++ src/core/shape.rs
@@ -1,10 +1,19 @@
 use crate::math::{Transform, Bounds3Df};
+use crate::core::SurfaceInteraction;
+use crate::ray::Ray;
 
 trait ShapeInterface {
     fn object_bound(&self) -> Bounds3Df;
     fn world_bound(&self, shape: &Shape) -> Bounds3Df {
         shape.object_to_world.transform_bounds(&self.object_bound())
     }
+    fn intersect(&self, shape: &Shape, ray: &Ray, test_alpha_texture: bool) -> (bool, f32, SurfaceInteraction);
+
+    fn intersect_p(&self, shape: &Shape, ray: &Ray, test_alpha_texture: bool) -> bool {
+        self.intersect(shape, ray, test_alpha_texture).0
+    }
+
+    fn area(&self) -> f32;
 }
 
 // Delete me !!!
@@ -15,6 +24,14 @@ struct ShapeImpl {
 impl ShapeInterface for ShapeImpl {
     fn object_bound(&self) -> Bounds3Df {
         Bounds3Df::default()
+    }
+
+    fn intersect(&self, shape: &Shape, ray: &Ray, test_alpha_texture: bool) -> (bool, f32, SurfaceInteraction) {
+        (false, ray.t_max.get(), SurfaceInteraction::delete_me_default())
+    }
+
+    fn area(&self) -> f32 {
+        0.0
     }
 }
 
@@ -43,6 +60,18 @@ impl Shape {
 
     pub fn world_bound(&self) -> Bounds3Df {
         self.shape_impl.world_bound(self)
+    }
+
+    fn intersect(&self, ray: &Ray, test_alpha_texture: bool) -> (bool, f32, SurfaceInteraction) {
+        self.shape_impl.intersect(self, ray, test_alpha_texture)
+    }
+
+    fn intersect_p(&self, ray: &Ray, test_alpha_texture: bool) -> bool {
+        self.shape_impl.intersect_p(self, ray, test_alpha_texture)
+    }
+
+    fn area(&self) -> f32 {
+        self.shape_impl.area()
     }
 }
 
